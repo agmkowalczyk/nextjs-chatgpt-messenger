9eb06ff30b593373969c1d004dc4ce9f7fb3b609
agmkowalczyk <agm.kowalczyk@gmail.com>
2024-01-12 10:09:50 +0100

    Add nextauth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,11 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import './globals.css'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from './auth/[...nextauth]'
+import SessionProvider from '@/components/SessionProvider'
 import SideBar from '@/components/SideBar'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,21 +14,25 @@ export const metadata: Metadata = {
   description: 'ChatGPT Messenger',
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions)
+
   return (
     <html lang='en'>
       <body className='flex'>
-        <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[15rem]'>
-          <SideBar />
-        </div>
+        <SessionProvider session={session}>
+          <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[15rem]'>
+            <SideBar />
+          </div>
 
-        {/* ClinetProvider - Notification */}
+          {/* ClinetProvider - Notification */}
 
-        <div className='bg-[#343541] flex-1'>{children}</div>
+          <div className='bg-[#343541] flex-1'>{children}</div>
+        </SessionProvider>
       </body>
     </html>
   )
